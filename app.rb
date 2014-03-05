dfb81fc4f624f8700686cd9306578cf879fefd90
Justin Kenyon <kenyonj@gmail.com>
2014-03-05 16:37:39 -0500

    SQL enabled pixtr app

--- app.rb
+++ app.rb
@@ -1,16 +1,35 @@
 require 'sinatra'
+require 'pg'
 
-GALLERIES = {
-  cats: %w(colonel_meow.jpg grumpy_cat.png),
-  dogs: %w(shibe.png)
-}
+database = PG.connect( dbname: 'photo_galleries' )
 
 get '/' do
   erb :index
 end
 
-get '/galleries/:gallery_name' do
-  @title = params[:gallery_name].capitalize
-  @images = GALLERIES[params[:gallery_name].to_sym]
+get '/galleries/:id' do
+  id = params[:id]
+  gallery = database.exec_params(
+    "SELECT * FROM galleries WHERE id = $1", 
+    [id]
+  ).first
+  @title = gallery["name"].capitalize
+  images = database.exec_params(
+    "SELECT * FROM images WHERE gallery_id = $1", 
+    [id]
+  )
+  @images = images.map { |image| image["url"] }
   erb :gallery
+end
+
+get '/newpicture' do
+  name = params["name"]
+  desc = params["desc"]
+  gallery_id = params["gallery_id"]
+  url = params["url"]
+  database.exec_params(
+    "INSERT INTO images(name, description, gallery_id, url) VALUES ($1, $2, $3, $4)",
+    [name, desc, gallery_id, url]
+  )
+  redirect to("/galleries/#{gallery_id}")
 end
